5b969d8723f15e001a3ad97e5f67da54dc32944e
musaliyah <asmusaliar@gmail.com>
2022-07-07 11:38:32 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+## A place to log your daily 9-5 schedule with color-coordination related to the time of day. Gray for the past, red for the present and green for the future. After pressing the right save button, the scheduler will keep your information even after the page is refreshed. 
+
+## In the process of making this scheduler I learned how to implement moment.js to create a scheduler that is reactive to the time of day, and utilized local storage to save information users type.
+
+### Link to repository: https://github.com/musaliyah/calendar-agenda
+### Link to live page: https://musaliyah.github.io/calendar-agenda/
+
+Usage: To use the application, users must type out details into their 9 to 5 schedule, and save the information by pressing the blue save button. After saving, when user's refresh the page, they can continue to reference what information they had written. To clear the scheduler, users can head into the developer tools, then to application, then to local storage where they can select "Ø" To delete what information they have stored. 
+
+![127 0 0 1_5500_index html](https://user-images.githubusercontent.com/105015206/177837783-df9b1963-ef78-421a-b381-2f12a5e92f1d.png)
+
+## Credits: Utilized Xandromus' starter code. 
+
+Xandromus' github: https://github.com/coding-boot-camp/super-disco
+
+No License 
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var presentTime = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(presentTime);
+//Creates a "presentTime" variable that stores the day-of-the-week, the mounth, the day of the month and year. 
+//Pulls from currentDay id in html and stores presentTime variable information into the jumbotron header.
 
 $(document).ready(function(){
 
@@ -9,11 +11,14 @@ $('.saveBtn').on('click', function (){
     localStorage.setItem(time, details);
     console.log(time , details);
 
-})
+}) 
+// References the saveBtn class, so that when the save button is clicked, then the function follows. 
+// Creates details and time variable to pull from the description class, and the ids to store information in variables in local storage and logs information into console. 
 
 function liveTime (){
 
     var now = moment().hour();
+    // creates a variable that utilizes moment.js to reference hours.
 
     $('.time-block').each(function(){
         var hours = parseInt($(this).attr('id').split('hour')[1]);
@@ -21,19 +26,24 @@ function liveTime (){
                 $(this).addClass('past');
                 $(this).removeClass('future');
                 $(this).removeClass('present');
-            }
+            } 
+            // references time-block class and creates a function. It then fetches the id attribute and grabs the id value and stores it in the hours variable. Compares the present time (stored in now variable) with the list of hours. If the listed hours are less than the present time, the past class is added (making the boxes gray to represent the past)
+
+            // recieved tutoring from askbcs to create the hours variable (Ben and Bianca)
             else if (hours === now) {
                 $(this).removeClass('past');
                 $(this).addClass('present');
                 $(this).removeClass('future');
 
             }
+            // similar to above, if the listed hour is the same as the present hour then the present class is added and the past, and fture classes are removed. This makes the present time-block red, referencing the css. 
             else {
                 $(this).removeClass('present');
                 $(this).removeClass('past');
                 $(this).addClass('future');
             }
         })
+            // if the time-block is not the present or past time, then the future class is added to turn the remaining time-blocks green. 
     
     } liveTime();
 $('#hour9 .description').val(localStorage.getItem('hour9'));
@@ -45,5 +55,6 @@ $('#hour14 .description').val(localStorage.getItem('hour14'));
 $('#hour15 .description').val(localStorage.getItem('hour15'));
 $('#hour16 .description').val(localStorage.getItem('hour16'));
 $('#hour17 .description').val(localStorage.getItem('hour17'));
+// keeps hour descriptions from local storage on the page (so it does not delete after its refreshed)
     
 })
